aa09b1778a0ec82c3279d2af25f903b88c384bba
jmanndev <jmanndev@gmail.com>
2017-01-24 00:57:46 +1100

    Files added

---
+++ Media Renamer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media_Renamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Renamer ren = new Renamer();
+            ren.run();
+        }
+    }
+}
---
+++ Media Renamer/Renamer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media_Renamer
+{
+    class Renamer
+    {
+        string folderPath =  "NEEDED";
+
+        public Renamer() { }
+
+        public void run()
+        {
+           DirectoryInfo d = new DirectoryInfo(folderPath);
+            
+            foreach (var file in d.GetFiles("*.mp4",SearchOption.AllDirectories))
+            {
+                Console.WriteLine(file.Name);
+
+                //Replaces title with filename
+                var newFile = TagLib.File.Create(file.FullName);
+                newFile.Tag.Title = file.Name;
+                newFile.Save();
+            }
+        }
+    }
+}
