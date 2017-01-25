7e850a1370e4d703380c96d03443ccf53b50f91d
jmanndev <jmanndev@gmail.com>
2017-01-25 14:01:04 +1100

    Strip '.' and excess chars

--- Media Renamer/Renamer.cs
+++ Media Renamer/Renamer.cs
@@ -9,23 +9,73 @@ namespace Media_Renamer
 {
     class Renamer
     {
-        string folderPath =  "NEEDED";
+        string folderPath = "F:\\Videos\\TV Shows\\Archer"; //remember to use \\ instead of \
 
         public Renamer() { }
 
         public void run()
         {
-           DirectoryInfo d = new DirectoryInfo(folderPath);
-            
-            foreach (var file in d.GetFiles("*.mp4",SearchOption.AllDirectories))
-            {
-                Console.WriteLine(file.Name);
+            DirectoryInfo d = new DirectoryInfo(folderPath);
 
-                //Replaces title with filename
+            //this is ugly.... find a way to do it all in one GETFILES call
+            foreach (FileInfo file in d.GetFiles("*.avi", SearchOption.AllDirectories))
+                rename(file);
+            foreach (FileInfo file in d.GetFiles("*.mp4", SearchOption.AllDirectories))
+                rename(file);
+            foreach (FileInfo file in d.GetFiles("*.mkv", SearchOption.AllDirectories))
+                rename(file);
+            foreach (FileInfo file in d.GetFiles("*.wmv", SearchOption.AllDirectories))
+                rename(file);
+        }
+
+        private void rename(FileInfo file)
+        {
+            string fileName = "";
+            fileName = correctName(file);
+            //Replaces title with filename
+            Console.WriteLine(fileName);
+           // setFileTitle(file, fileName);
+
+            string newFileName = file.DirectoryName + "\\" + fileName;
+            File.Move(file.FullName, newFileName);
+
+        }
+
+        private string correctName(FileInfo file)
+        {
+            string extension = Path.GetExtension(file.FullName);
+            string fileName = Path.GetFileNameWithoutExtension(file.FullName);
+            fileName = removeJunk(fileName);
+
+            return fileName + extension;
+        }
+
+        private string removeJunk(string input)
+        {
+            string fileName = input;
+
+            //NEED TO CORRECT THIS
+            //handle SXXEXX instead of numbers
+            char[] numbers = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            int index = fileName.LastIndexOfAny(numbers);
+            if (index > 0)
+            {
+                fileName = fileName.Substring(0, index + 1);
+            }
+
+            fileName = fileName.Replace('.', ' ');  //removes fullstops
+            return fileName;
+        }
+
+        private void setFileTitle(FileInfo file, string fileName)
+        {
+            try
+            {
                 var newFile = TagLib.File.Create(file.FullName);
-                newFile.Tag.Title = file.Name;
+                newFile.Tag.Title = fileName;
                 newFile.Save();
             }
+            catch (Exception) { }
         }
     }
 }
